3b84478685577c3213f75ecdc5663418811075c3
kyranxx <blanarikdaniel@gmail.com>
2025-01-21 20:36:09 +0100

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const TWITTER_API_ENDPOINTS = {
+  AUTH: 'https://api.twitter.com/oauth/authenticate',
+  TWEETS: 'https://api.twitter.com/2/users/:id/tweets',
+  COMMENTS: 'https://api.twitter.com/2/users/:id/replies'
+};
