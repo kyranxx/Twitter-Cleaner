053669745d54b50a7d6cbda7b53cd3eea4cc64ae
kyranxx <blanarikdaniel@gmail.com>
2025-01-21 20:36:25 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TwitterCleaner from './components/TwitterCleaner';
+
+const App = () => {
+  return (
+    <div>
+      <TwitterCleaner />
+    </div>
+  );
+};
+
+export default App;
