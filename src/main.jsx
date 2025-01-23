0c2cfaeb47a6a3c15e2374281bd8231e4921f9b9
kyranxx <blanarikdaniel@gmail.com>
2025-01-23 23:26:08 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TwitterCleaner from './components/TwitterCleaner/TwitterCleaner'
-import './styles.css'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TwitterCleaner />
+    <App />
   </React.StrictMode>,
 )
