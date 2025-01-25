6c3ed52226b776ddf9ca63394a4a4de34a702568
kyranxx <blanarikdaniel@gmail.com>
2025-01-25 21:29:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-// src/App.jsx
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TwitterCleaner from './components/TwitterCleaner/TwitterCleaner';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <TwitterCleaner />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TwitterCleaner />} />
+        <Route path="/callback" element={<TwitterCleaner />} />
+      </Routes>
+    </Router>
   );
 }
 
