24709b3b489ab85e8a284c851d63c6751fcf8103
kyranxx <blanarikdaniel@gmail.com>
2025-01-25 21:46:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TwitterCleaner from './components/TwitterCleaner/TwitterCleaner';
 import Dashboard from './components/TwitterCleaner/Dashboard';
 
+// Add future flags for React Router v7
+const router = {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true
+  }
+};
+
 function App() {
   return (
-    <Router>
+    <Router {...router}>
       <Routes>
         <Route path="/" element={<TwitterCleaner />} />
         <Route path="/callback" element={<TwitterCleaner />} />
