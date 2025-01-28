35dfd6739faa1e4450e11ea02626645ef23f6c67
kyranxx <blanarikdaniel@gmail.com>
2025-01-28 22:37:40 +0100

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  clientId: import.meta.env.VITE_TWITTER_CLIENT_ID,
+  clientId: 'SmFPMml6WnoOekNWWDQ4bEpSd2I6MTpjaQ',
   redirectUri: 'https://twitter-cleaner-2.vercel.app/callback'
 };
