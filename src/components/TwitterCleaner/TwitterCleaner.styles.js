1ab99616592a82e9a435994843177dfdbbb7d628
kyranxx <blanarikdaniel@gmail.com>
2025-02-01 21:43:28 +0100

    Create TwitterCleaner.styles.js

---
+++ src/components/TwitterCleaner/TwitterCleaner.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 20px;
+  gap: 20px;
+  
+  h1 {
+    margin-bottom: 20px;
+    color: #1da1f2;
+  }
+
+  p {
+    margin: 10px 0;
+  }
+`;
