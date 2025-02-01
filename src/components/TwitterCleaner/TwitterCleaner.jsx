6247fb6b2e570025836fee3ff6fd21635445cb34
kyranxx <blanarikdaniel@gmail.com>
2025-02-01 21:31:53 +0100

    Update TwitterCleaner.jsx

--- src/components/TwitterCleaner/TwitterCleaner.jsx
+++ src/components/TwitterCleaner/TwitterCleaner.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { TWITTER_CONFIG, generateTwitterAuthUrl, validateOAuthState, getStoredCodeVerifier, clearOAuthData } from '../../config/twitter';
-import LoadingButton from '../LoadingButton/LoadingButton'; // Changed to relative path
+import LoadingButton from '../LoadingButton'; // Changed import path
 import { Container } from './TwitterCleaner.styles';
 
 const TwitterCleaner = () => {
